e90f590e0b9fe7878d375167ab5b8e6305abf7fc
fbonilla_ <105347728+fbonilla11@users.noreply.github.com>
2022-11-04 10:02:41 +0100

    Update ejercicio03.js

---
+++ js/ejercicio03.js
@@ -0,0 +1,33 @@
+var datos = document.getElementById("datos");
+var titulo = document.getElementById("titulo");
+var info = document.getElementById("info")
+
+let body = document.querySelector('#body');
+document.addEventListener('mousemove', raton);
+
+let body1 = document.querySelector('#body');
+document.addEventListener('keydown', teclado);
+
+function raton(e) {
+    titulo.innerHTML = "Ratón";
+    datos.innerText = `
+    Navegador [${e.screenX}, ${e.screenY}]
+    Página [${e.clientX}, ${e.clientY}]`;
+    info.style.backgroundColor = null;
+}
+
+function teclado(e1) {
+    titulo.innerHTML = "Teclado";
+    datos.innerText = `
+    Caracter [${e1.key}]
+    Código [${e1.code}]`;
+    info.style.backgroundColor = "#CCE6FF";
+}
+
+function cambiarColor(){
+    info.style.backgroundColor = "#FFFFCC";
+}
+
+
+
+
